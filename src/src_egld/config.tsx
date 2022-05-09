2a589268e6f6e4a5b4d495e9152718f43b376c9a
constantinp2022 <pricopecostel98@yahoo.com>
2022-05-09 11:02:37 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/src_egld/config.tsx
@@ -0,0 +1,4 @@
+export const contractAddressEGLD =
+  'erd1qqqqqqqqqqqqqpgq03s6pnapp0sh86h6wfpau234zarz05093zms7pfxuj';
+
+export const dAppName = 'Bridge between Elrond and Harmony';
---
+++ src/src_hrc/config.tsx
@@ -0,0 +1,4 @@
+export const contractAddress =
+  'erd1qqqqqqqqqqqqqpgq03s6pnapp0sh86h6wfpau234zarz05093zms7pfxuj';
+
+export const dAppName = 'Bridge between Elrond and Harmony';
